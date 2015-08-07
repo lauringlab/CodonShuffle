927f02a515a3e4412eaf6e69efb71d7883534a35
danielmjorge <dmacedod@umich.edu>
2015-08-07 16:14:55 -0400

    Install dependecies

---
+++ install_dependencies.py
@@ -0,0 +1,10 @@
+import subprocess
+subprocess.call(['pip', 'install', 'numpy'])
+subprocess.call(['pip', 'install', 'scipy'])
+subprocess.call(['pip', 'install', 'matplotlib'])
+subprocess.call(['pip', 'install', 'pandas'])
+subprocess.call(['pip', 'install', 'statsmodels'])
+subprocess.call(['pip', 'install', 'patsy'])
+subprocess.call(['pip', 'install', 'Bio'])
+subprocess.call(['pip', 'install', 'ggplot'])
+print "Ready to run CodonShuffle"
