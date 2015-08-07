083e7e7b112515d9c87cefdc5557bcb829e2f46c
danielmjorge <dmacedod@umich.edu>
2015-08-07 16:56:34 -0400

    New

--- install_dependencies.py
+++ install_dependencies.py
@@ -5,6 +5,7 @@ subprocess.call(['pip', 'install', 'matplotlib'])
 subprocess.call(['pip', 'install', 'pandas'])
 subprocess.call(['pip', 'install', 'statsmodels'])
 subprocess.call(['pip', 'install', 'patsy'])
-subprocess.call(['pip', 'install', 'Bio'])
+subprocess.call(['pip', 'install', 'biopython'])
+#subprocess.call(['easy_install', '-f', 'http://biopython.org/DIST/', 'biopython'])
 subprocess.call(['pip', 'install', 'ggplot'])
 print "Ready to run CodonShuffle"
