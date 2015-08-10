9ccf6e834e14accf786b10fe56976d34298500fc
danielmjorge <dmacedod@umich.edu>
2015-08-10 16:33:05 -0400

    New

--- install_dependencies.py
+++ install_dependencies.py
@@ -1,11 +1,15 @@
 import subprocess
-subprocess.call(['pip', 'install', 'numpy'])
-subprocess.call(['pip', 'install', 'scipy'])
-subprocess.call(['pip', 'install', 'matplotlib'])
-subprocess.call(['pip', 'install', 'pandas'])
-subprocess.call(['pip', 'install', 'statsmodels'])
-subprocess.call(['pip', 'install', 'patsy'])
-subprocess.call(['pip', 'install', 'biopython'])
+subprocess.call(['pip', 'install', 'numpy']) # Install Numpy
+subprocess.call(['pip', 'install', 'scipy']) # Install Scipy
+subprocess.call(['pip', 'install', 'matplotlib']) # Install Matplotlib
+subprocess.call(['pip', 'install', 'pandas']) # Install Pandas
+subprocess.call(['pip', 'install', 'statsmodels']) # Install Statsmodels
+subprocess.call(['pip', 'install', 'patsy']) # Install Patsy
+subprocess.call(['pip', 'install', 'biopython']) # Install Biopython
 #subprocess.call(['easy_install', '-f', 'http://biopython.org/DIST/', 'biopython'])
-subprocess.call(['pip', 'install', 'ggplot'])
+subprocess.call(['pip', 'install', 'ggplot']) # Install Ggplot
+subprocess.call(['./lib/EMBOSS-6.6.0/configure', '--without-x']) # Install Emboss
+os.chdir('./lib/EMBOSS-6.6.0/') #Install Emboss
+subprocess.call(['make']) #Install Emboss
 print "Ready to run CodonShuffle"
+
