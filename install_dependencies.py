d0ca985ac68abeed750d13cb534ec0bb9ae6b29a
danielmjorge <dmacedod@umich.edu>
2015-08-11 15:34:20 -0400

    New

--- install_dependencies.py
+++ install_dependencies.py
@@ -9,8 +9,8 @@ subprocess.call(['pip', 'install', 'patsy']) # Install Patsy
 subprocess.call(['pip', 'install', 'biopython']) # Install Biopython
 #subprocess.call(['easy_install', '-f', 'http://biopython.org/DIST/', 'biopython'])
 subprocess.call(['pip', 'install', 'ggplot']) # Install Ggplot
-subprocess.call(['./lib/EMBOSS-6.6.0/configure', '--without-x']) # Install Emboss
 os.chdir('./lib/EMBOSS-6.6.0/') #Install Emboss
-subprocess.call(['make']) #Install Emboss
+subprocess.call(['./lib/EMBOSS-6.6.0/configure', '--without-x']) # Install Emboss
+ssubprocess.call(['make']) #Install Emboss
 print "Ready to run CodonShuffle"
 
